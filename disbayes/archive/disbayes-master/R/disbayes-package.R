432f0f69c867329b3e750497d692d69eb4255d82
Belen <36194885+BelenZapata85@users.noreply.github.com>
2020-10-28 13:07:03 +1100

    organised folders

---
+++ disbayes/archive/disbayes-master/R/disbayes-package.R
@@ -0,0 +1,26 @@
+#' The 'disbayes' package.
+#'
+#' @description Bayesian evidence synthesis for chronic disease epidemiology
+#'
+#' @docType package
+#' @name disbayes-package
+#' @useDynLib disbayes, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#' @importFrom stats lm sd rlnorm rnorm
+#' @importFrom mgcv smoothCon jagam s
+#'
+#' @references
+#' Stan Development Team (2019). RStan: the R interface to Stan. R package version 2.19.2. https://mc-stan.org
+#'
+NULL
+
+##' Ischaemic heart disease in Bristol
+##'
+##' @format TODO 
+##' 
+##' @source Global Burden of Disease study 
+##' 
+##' @keywords datasets
+"ihdbristol"
